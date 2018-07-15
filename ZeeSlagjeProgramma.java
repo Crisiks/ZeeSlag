9e2b7b1c1843b056fd5658c6dfb2c02b6be6ea18
Cris <crispijn.sleeboom@gmail.com>
2018-07-15 18:22:24 +0200

    commit 3

--- ZeeSlagjeProgramma.java
+++ ZeeSlagjeProgramma.java
@@ -10,22 +10,29 @@ class ZeeSlagjeProgramma {
 	public static void main(String[] args) {
 
 	//	Maakt een zee aan: 0 = water, 1 = boot, 2 = gezonkenboot	
-		int[] Zee = new int[5];
-		Zee[3] = 1;
-		Zee[4] = 1;
-		Zee[5] = 1;
+		int[][] Zee = new int[3][6];
+		Boot boot1 = new Boot();
+		Boot boot2 = new Boot();
+		Boot boot3 = new Boot();
+		Zee[0][1] = boot1.boot;
+		Zee[0][0] = boot1.boot;
+		Zee[1][5] = boot2.boot;
+		Zee[2][3] = boot3.boot;
 		
-		int[] Schot = new int[10];
+		int[] SchotOpRij = new int[3];
+		int[] SchotOpKolom = new int[6];
 		int i = 0;
 		int[] Aantalkeergeraakt = new int [1]; //Waarom moet dit specifiek een array zijn, geen integer?
 		Aantalkeergeraakt[0] = 0;
 		
-		while (Aantalkeergeraakt[0] < 3) {
+		while (Aantalkeergeraakt[0] < 4) {
 		
-			printZee (Zee);
-			schieten(Schot); 
-			int Doelwit = Schot[i];
-			raken(Doelwit, Zee, Aantalkeergeraakt);
+			printZee (Zee);			
+			schietOpRij(SchotOpRij);
+			schietOpKolom(SchotOpKolom);
+			int DoelwitOpRij = SchotOpRij[i];
+			int DoelwitOpKolom = SchotOpKolom[i];
+			raken(DoelwitOpRij, DoelwitOpKolom, Zee, Aantalkeergeraakt);
 			}
 		
 		if (Aantalkeergeraakt[0] == 3) {
@@ -35,46 +42,63 @@ class ZeeSlagjeProgramma {
 		
 	}
 	
-	public static void printZee (int[] Zee) {
-		int i = 0;
-		for (i = 0; i < Zee.length; i++) {
-//		System.out.println("Element at index " + i + " : "+ Zee[i]);
-			if(Zee[i] == 0) {
-				System.out.print("~");
-			}	
-			if(Zee[i] == 1) {
-				System.out.print("-");
-			}
-			if(Zee[i] == -1) {
-				System.out.print("o");
-			}
-			if(Zee[i] == 2) {
-				System.out.print("x");
+	public static void printZee (int[][] Zee) {
+		for(int i = 0; i < Zee.length; i++) {
+		for(int j = 0; j < Zee[i].length; j++) {
+				if(Zee[i][j] == 0) {
+					System.out.print("~");
+					}	
+				if(Zee[i][j] == 1) {
+					System.out.print("-");
+					}
+				if(Zee[i][j] == -1) {
+					System.out.print("o");
+					}
+				if(Zee[i][j] == 2) {
+					System.out.print("x");
+					}
+				if(j == 5) {
+					System.out.println(" ");
+				}
 			}	
 		}
 		System.out.println(" ");
 	}
-
-	public static int schieten (int[] Schot){
+	
+	public static int schietOpRij (int[] SchotOpRij){
 		int i = 0;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Schiet op positie: ");
-		Schot[i] = input.nextInt();
-		System.out.println("Je hebt geschoten op: "+ Schot[i]);
-		return Schot[i];
+		System.out.println("Schiet op rij: ");
+		SchotOpRij[i] = input.nextInt();
+//		System.out.println("Je hebt geschoten op: "+ SchotOpRij[i]);
+		return SchotOpRij[i];
 	}
 	
-	public static void raken (int Doelwit, int[] Zee, int[] Aantalkeergeraakt) {
-		if(Zee[Doelwit] == 0) {
+	public static int schietOpKolom (int[] SchotOpKolom){
+		int i = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Schiet op kolom: ");
+		SchotOpKolom[i] = input.nextInt();
+//		System.out.println("Je hebt geschoten op: "+ SchotOpKolom[i]);
+		return SchotOpKolom[i];
+	}
+
+	public static void raken (int DoelwitOpRij, int DoelwitOpKolom, int[][] Zee, int[] Aantalkeergeraakt) {
+		if(Zee[DoelwitOpRij][DoelwitOpKolom] == 0) {
 			System.out.println("Mis!");
-			Zee[Doelwit] = -1;
+			Zee[DoelwitOpRij][DoelwitOpKolom] = -1;
 		}	
-		if(Zee[Doelwit] == 1) {
+		if(Zee[DoelwitOpRij][DoelwitOpKolom] == 1) {
 			System.out.println("Raak!");
-			Zee[Doelwit] = 2;
+			Zee[DoelwitOpRij][DoelwitOpKolom] = 2;
 			Aantalkeergeraakt[0]++; //Waarom moet dit specifiek een array zijn, geen integer?
 			System.out.println("Je hebt al " +Aantalkeergeraakt[0] + " doelwit geraakt!");
+			System.out.println(" ");
 		}
 	}
-	
 }	
+
+class Boot{
+	int boot = 1;
+	String telwoord;
+}
